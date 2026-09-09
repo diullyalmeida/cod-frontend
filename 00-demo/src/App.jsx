7b2777838a64b2c627c3e6822c41987718d4ecb2
diully <diully_almeida@estudante.sesisenai.org.br>
2026-09-09 17:13:48 -0300

    primeiro projeto do git

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className="cont-app">
+    <h1>Trampando com React</h1>
+    <h2>Onde posso trampar com o dev</h2>
+  <p>Empresas de Tecnologia e Software Houses: Atuação direta no desenvolvimento de sistemas web, aplicações SaaS, e plataformas digitais para clientes diversos.</p>
+ 
+  <p>Agências Digitais e de Marketing: Criação e manutenção de sites, landing pages, e-commerces e portais interativos para marcas e clientes da agência.</p>
+  <p>Startups: Desenvolvimento ágil de produtos mínimos viáveis (MVPs), manutenção de plataformas e criação de novas funcionalidades em times enxutos.</p>
+  <p>Departamentos de TI em Grandes Empresas: Atuação no desenvolvimento e manutenção de sistemas internos (intranets, painéis administrativos) em setores como bancos, varejo, indústria e saúde.</p>
+  <p>Setor Público: Suporte e desenvolvimento de portais governamentais, sistemas de atendimento ao cidadão e transparência.</p>
+  <p>Trabalho Autônomo / Freelancer: Prestação de serviços diretos para pequenos e médios negócios (criação de sites, e-commerces e integrações de API) ou prestação de serviços por plataformas globais.</p>
+  <ul>
+    <li>Desenvolvedor Front-End: Focado na interface visual e interatividade (HTML, CSS, JavaScript, React/Vue).</li>
+    <li>Desenvolvedor Back-End: Focado na lógica do servidor, banco de dados e APIs (Node.js, PHP, Python, SQL).</li>
+    <li>Desenvolvedor Full-Stack Júnior: Atuando ponta a ponta na criação de pequenas aplicações web.</li>
+    <li>Webmaster / Mantenedor de Sites: Gestão, atualização e otimização de performance/SEO de plataformas existentes.</li>
+    <li>Desenvolvedor de E-commerce / Plataformas (ex: Shopify, WooCommerce, Magento): Focado na criação, customização e manutenção de lojas virtuais, configurando meios de pagamento, integração de frete e layout.</li>
+  
+  </ul>
+  </div>
+  )
+  
+}
+
+export default App
